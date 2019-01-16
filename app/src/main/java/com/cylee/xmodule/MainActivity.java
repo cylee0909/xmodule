6c83003cb9d1791e155a979dfe723656748db05e
lichongyang <lichongyang@zuoyebang.com>
2019-01-16 08:39:47 +0000

    modify the demo

--- app/src/main/java/com/cylee/xmodule/MainActivity.java
+++ app/src/main/java/com/cylee/xmodule/MainActivity.java
@@ -2,13 +2,13 @@ package com.cylee.xmodule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import com.cylee.testapi.SubModuleApi;
+import com.cylee.testapi.SubModuleApi;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        SubModuleApi.test();
+        SubModuleApi.test();
     }
 }
